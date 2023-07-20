21493b35f022873548fd7907a9da1046de066c8a
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-07-20 11:12:16 +0500

    Update KadanesAlgorithm.java

--- KadanesAlgorithm.java
+++ KadanesAlgorithm.java
@@ -1,20 +1,20 @@
 public class KadanesAlgorithm {
     public static void kadanesAlgorithm (int[]numbers){
         int currentSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE; // - Infinity
 
         for (int i=0 ; i< numbers.length ; i++){
-                currentSum = currentSum + numbers[i] ;
+                currentSum = currentSum + numbers[i] ;     // C-S will be adding into Numbers-Array.
 
-            if (currentSum == -1)
+            if (currentSum == -1)   // CS = negativeIntegers - update it to zero value.
                 currentSum = 0 ;
 
-            maxSum = Math.max(currentSum,maxSum) ;
+            maxSum = Math.max(currentSum,maxSum) ;     // Update maxSum value.
         }
         System.out.println("The Largest Sub Array Sum Is : "+maxSum);
     }
     public static void main(String[] args) {
         int []numbers ={0,-1,5,7,-5,-4} ;
-            kadanesAlgorithm(numbers);
+            kadanesAlgorithm(numbers); // Passing Array By Reference.
     }
 }
